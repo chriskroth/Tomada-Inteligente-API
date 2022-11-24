408f491b3754c0d17d4ebb117494d26415fdb894
Chris <christiankroth@gmail.com>
2022-11-23 21:56:35 -0300

    Adicionado índice de UNIQUE para a coluna 'numero_serial'.

--- database/migrations/2022_09_13_022436_create_plugs_table.php
+++ database/migrations/2022_09_13_022436_create_plugs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
     {
         Schema::create('plugs', function (Blueprint $table) {
             $table->id();
-            $table->string('serial_number', 80)->comment("Número de serie único para cada tomada.");
+            $table->string('serial_number', 80)->unique()->comment("Número de serie único para cada tomada.");
             $table->string('name', 80)->nullable(true);
             $table->integer('power')->default(100)->comment("Potência que deve ser liberada. 0% a 100%");
             $table->float('consumption')->nullable(true)->comment("Consumo de energia.");
